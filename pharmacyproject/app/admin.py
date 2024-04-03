1bd49e1fef97f5b3727f43df8fe343b2f3c348ae
aayushipatel723 <aayushipatel723@gmail.com>
2024-04-03 11:47:19 +0530

    HealthHub

---
+++ pharmacyproject/app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Contact,Medicines,ProductItems,MyOrders
+
+# Register your models here.
+admin.site.register(Contact)
+admin.site.register(Medicines)
+admin.site.register(ProductItems)
+admin.site.register(MyOrders)
